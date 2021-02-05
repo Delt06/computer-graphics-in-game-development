927f9083e19dea1ace645f621bbfedc457243af9
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-02-05 13:27:42 +0300

    Add lambertian shading

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -33,7 +33,9 @@ void cg::renderer::ray_tracing_renderer::init()
 	camera->set_z_far(settings->camera_z_far);
 	camera->set_z_near(settings->camera_z_near);
 
-	
+	float3 light_position = float3(0, 1.58f, -0.03f);
+	float3 light_color = float3(0.78f, 0.78f, 0.78f);
+	lights.push_back({ light_position, light_color });
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -50,10 +52,23 @@ void cg::renderer::ray_tracing_renderer::render()
 						  ray.direction.z * 0.5f + 0.5f};
 		return payload;
 	};
-	raytracer->closest_hit_shader = [](const ray& ray, payload& payload,
+	raytracer->closest_hit_shader = [&](const ray& ray, payload& payload,
 									   const triangle<cg::vertex>& triangle) 
 	{
-		payload.color = cg::color::from_float3(triangle.ambient);
+		float3 result_color = triangle.emissive;
+		float3 position = ray.position + ray.direction * payload.t;
+		float3 normal = payload.bary.x * triangle.na +
+						payload.bary.y * triangle.nb + 
+						payload.bary.z * triangle.nc;
+
+		for (auto& light : lights)
+		{
+			cg::renderer::ray to_light(position, light.position - position);
+			result_color += triangle.diffuse * light.color *
+							std::max(0.f, dot(normal, to_light.direction));
+		}
+
+		payload.color = cg::color::from_float3(result_color);
 		return payload;
 	};
 
